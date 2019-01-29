1e8568e291b43777bc2ef23c334c2ec260c9fc45
pdiakumis <pdiakumis@unimelb.edu.au>
2019-01-29 20:49:09 +1100

    setup R pkg structure

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# bcr 0.0.0.1
+
+This package will hopefully help with comparing 
+[bcbio](https://github.com/bcbio/bcbio-nextgen) runs.
---
+++ setup-pkg.R
@@ -0,0 +1,47 @@
+library(usethis)
+
+# Follow README at https://github.com/r-lib/usethis
+
+# Creates following files/directories:
+# - .Rbuildignore
+# - .Rproj
+# - DESCRIPTION
+# - man/
+# - NAMESPACE
+# - R/
+create_package(path = "/Users/pdiakumis/Desktop/projects/umccr/bcr")
+
+# - Sets License field in DESCRIPTION
+# - Creates LICENSE and LICENSE.md files
+use_mit_license(name = "Peter Diakumis")
+
+# - Include a package in the DESCRIPTION under e.g. Imports (default)
+use_package("dplyr", type = "Imports")
+use_package("ggplot2")
+use_package("purrr")
+use_package("tibble")
+use_package("tidyr")
+
+# - Set Roxygen field in DESCRIPTION to 'list(markdown = TRUE)'
+# - Set RoxygenNote field in DESCRIPTION to '6.1.1'
+use_roxygen_md()
+
+# - Generates README.Rmd so that you can mingle code with prose
+use_readme_rmd()
+
+# - Generates NEWS.md to keep track of new features added or major issues resolved
+use_news_md()
+
+# - Adds 'testthat' to Suggests field in DESCRIPTION
+# - Creates 'tests/testthat/'
+# - Writes 'tests/testthat.R'
+# - Writes 'tests/testthat/test-my-test.R'
+use_test("my-test1")
+
+# Allows you and users to use '%>%' without explicitly loading dplyr/magrittr
+# Adds 'magrittr' to Imports field in DESCRIPTION
+# Writes 'R/utils-pipe.R'
+use_pipe()
+
+# use git if not already initiated
+use_git()
