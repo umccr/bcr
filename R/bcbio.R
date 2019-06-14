89dabd7f82a1fd75c405f69f4580888646082870
pdiakumis <pdiakumis@unimelb.edu.au>
2019-06-14 12:23:12 +1000

    generate sample list

--- R/bcbio.R
+++ R/bcbio.R
@@ -3,7 +3,7 @@ require(tidyverse)
 require(yaml)
 
 # Given a bcbio config directory, read in <datetimestamp_project_batch-merged.yaml>.
-# - 'details' contains two samples
+# - 'details' contains X samples
 # - each sample contains:
 #   - algorithm (aligner, svcaller, variantcaller, fusion_caller)
 #   - analysis (variant2, RNA-seq)
@@ -17,8 +17,8 @@ x <- "nogit/data/2019-02-01T0241_Cromwell_WGS_CUP-Pairs8-merged.yaml"
 # x <- "nogit/data/2019-05-31T1359_kym_WTS-merged.yaml"
 stopifnot(file.exists(x))
 
-read_bcbio_config <- function(x) {
-
+bcbio_sample <- function(s) {
+  # given a subelement from 'details', extract stuff
 
   get_varcallers <- function(samp) {
     snv <- samp[["algorithm"]][["variantcaller"]]
@@ -38,50 +38,47 @@ read_bcbio_config <- function(x) {
     snv_and_sv
   }
 
-  bcbio_sample <- function(s) {
-    # given a subelement from 'details', extract stuff
-
-    # s <- samples[[1]]
-    algo <- s[["algorithm"]]
-    analysis <- s[["analysis"]]
-    nm <- s[["description"]]
-    meta <- s[["metadata"]]
-    varcallers <- NULL
-    if (analysis == "variant2") {
-      varcallers <- get_varcallers(s)
-    }
-    # if RNA: fusion_caller
-    # if DNA: variantcaller, svcaller
-    structure(
-      list(
-        sample_name = nm,
-        batch = meta[["batch"]],
-        phenotype = meta[["phenotype"]],
-        analysis = analysis,
-        aligner = algo[["aligner"]],
-        varcallers = varcallers,
-        fusioncaller = algo[["fusion_caller"]]),
-      class = "bcbio_sample"
-    )
+  # s <- samples[[1]]
+  algo <- s[["algorithm"]]
+  analysis <- s[["analysis"]]
+  nm <- s[["description"]]
+  meta <- s[["metadata"]]
+  varcallers <- NULL
+  if (analysis == "variant2") {
+    varcallers <- get_varcallers(s)
   }
 
-  bcbio_batch <- function(sl) {
-    # given a sample list, output a list of batches with
-    # tumor/normal pairs. e.g., for a file with
-    # 10 T/N pairs, output a list of 10 batches
+  l <- list(sample_name = nm,
+            batch = meta[["batch"]],
+            phenotype = meta[["phenotype"]],
+            analysis = analysis,
+            aligner = algo[["aligner"]],
+            varcallers = varcallers,
+            fusioncaller = algo[["fusion_caller"]])
+  setNames(list(l), nm)
+}
 
-    # - gather elements with the same batch name
+bcbio_batch <- function(sl) {
+  # given a sample list, output a list of batches with
+  # tumor/normal pairs. e.g., for a file with
+  # 10 T/N pairs, output a list of 10 batches
+
+  sample_batch_df <- purrr::map(sl, function(s) list(sample_name = s$sample_name, batch = s$batch)) %>%
+    dplyr::bind_rows()
+  batches <- unique(sample_batch_df$batch)
+  purrr::map(batches, function(b) sample_batch_df$sample_name[sample_batch_df$batch == b]) %>%
+    purrr::set_names(batches)
 
 
-  }
+
+}
+
+
+read_bcbio_config <- function(x) {
 
   conf <- yaml::read_yaml(x)
-
-  res <- list()
-  samples <- conf$details
-  res$n_samples <- length(samples)
-
   final_dir <- normalizePath(conf$upload$dir, mustWork = F)
-  lapply(samples, bcbio_sample)
-
+  samples <- purrr::map(conf$details, function(s) bcbio_sample(s)[[1]]) %>%
+    purrr::set_names(purrr::map_chr(., "sample_name"))
+  bcbio_batch(samples)
 }
