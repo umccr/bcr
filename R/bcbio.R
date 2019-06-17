f14895d8df02ca027532743f6afd8578618db850
pdiakumis <pdiakumis@unimelb.edu.au>
2019-06-17 23:03:08 +1000

    generate bcbio single batch info

--- R/bcbio.R
+++ R/bcbio.R
@@ -14,7 +14,6 @@ require(yaml)
 
 
 x <- "nogit/data/2019-02-01T0241_Cromwell_WGS_CUP-Pairs8-merged.yaml"
-# x <- "nogit/data/2019-05-31T1359_kym_WTS-merged.yaml"
 stopifnot(file.exists(x))
 
 bcbio_sample <- function(s) {
@@ -38,7 +37,6 @@ bcbio_sample <- function(s) {
     snv_and_sv
   }
 
-  # s <- samples[[1]]
   algo <- s[["algorithm"]]
   analysis <- s[["analysis"]]
   nm <- s[["description"]]
@@ -52,6 +50,7 @@ bcbio_sample <- function(s) {
             batch = meta[["batch"]],
             phenotype = meta[["phenotype"]],
             analysis = analysis,
+            genome_build = s[["genome_build"]],
             aligner = algo[["aligner"]],
             varcallers = varcallers,
             fusioncaller = algo[["fusion_caller"]])
@@ -59,26 +58,67 @@ bcbio_sample <- function(s) {
 }
 
 bcbio_batch <- function(sl) {
-  # given a sample list, output a list of batches with
-  # tumor/normal pairs. e.g., for a file with
-  # 10 T/N pairs, output a list of 10 batches
+  # given a T/N sample list, output batch info
 
-  sample_batch_df <- purrr::map(sl, function(s) list(sample_name = s$sample_name, batch = s$batch)) %>%
+  stopifnot(length(sl) == 2)
+
+  d <- purrr::map(sl, function(s) {
+    tibble::tribble(
+      ~sample_name, ~batch, ~phenotype, ~analysis, ~aligner, ~genome_build, ~varcallers,
+      s$sample_name, s$batch, s$phenotype, s$analysis, s$aligner, s$genome_build, s$varcallers,
+    )}) %>%
     dplyr::bind_rows()
-  batches <- unique(sample_batch_df$batch)
-  purrr::map(batches, function(b) sample_batch_df$sample_name[sample_batch_df$batch == b]) %>%
-    purrr::set_names(batches)
 
+  stopifnot(identical(d[1, c("batch", "analysis", "genome_build", "aligner", "varcallers")],
+                      d[2, c("batch", "analysis", "genome_build", "aligner", "varcallers")]))
 
+  b <- list()
+  b$batch_name <- d$batch[1]
+  b$tumor_name <- d$sample_name[d$phenotype == "tumor"]
+  b$normal_name <- d$sample_name[d$phenotype == "normal"]
+  b$analysis_type <- d$analysis[1]
+  b$genome_build <- d$genome_build[1]
+  b$aligner <- d$aligner[1]
+  b$varcallers <- d$varcallers[[1]]
 
+  b
 }
 
+bcbio_vcfs <- function(batch) {
+  # Need batch/tumor/normal names, final path
+  # Construct paths to VCFs
+  bn <- batch[["batch_name"]]
+  nn <- batch[["normal_name"]]
+  tn <- batch[["tumor_name"]]
+  fd <- batch[["final_dir"]]
+  dd <- batch[["date_dir"]]
+  vc <- batch[["varcallers"]]
+
+  # if germ-snv: <final>/<date_dir>/<normal>-germline-<caller>-annotated.vcf.gz
+  # if soma-snv: <final>/<date_dir>/<batch>-<caller>-annotated.vcf.gz
+  # if soma-sv:  <final>/<tumor>/<batch>-sv-prioritize-manta.vcf.gz
+  vc %>%
+    dplyr::mutate(
+      fpath = dplyr::case_when(
+        caller_type == "germ-snv" ~ file.path(dd, paste0(nn, "-germline-", value, "-annotated.vcf.gz")),
+        caller_type == "soma-snv" ~ file.path(dd, paste0(bn, "-", value, "-annotated.vcf.gz")),
+        caller_type == "soma-sv" ~ file.path(fd, tn, paste0(bn, "-sv-prioritize-", value, ".vcf.gz")),
+        TRUE ~ "XXX"))
+}
 
 read_bcbio_config <- function(x) {
 
   conf <- yaml::read_yaml(x)
-  final_dir <- normalizePath(conf$upload$dir, mustWork = F)
-  samples <- purrr::map(conf$details, function(s) bcbio_sample(s)[[1]]) %>%
+  final_dir <- "/Users/pdiakumis/Desktop/projects/umccr/woof/nogit/data/f1/final"
+  # final_dir <- normalizePath(conf[["upload"]][["dir"]], mustWork = F)
+  date_dir <- Sys.glob(file.path(final_dir, paste0("*", conf[["fc_name"]])))
+  samples <- purrr::map(conf[["details"]], function(s) bcbio_sample(s)[[1]]) %>%
     purrr::set_names(purrr::map_chr(., "sample_name"))
-  bcbio_batch(samples)
+  batch <- bcbio_batch(samples)
+  batch$final_dir <- final_dir
+  batch$date_dir <- date_dir
+  batch$varcallers <- bcbio_vcfs(batch)
+  batch
 }
+
+batch <- read_bcbio_config(x)
