df96764309f60b4a5017c6aef706eaaa7053d415
pdiakumis <pdiakumis@unimelb.edu.au>
2019-10-12 19:03:50 +1100

    bump v0.1.2

--- bump.sh
+++ bump.sh
@@ -3,9 +3,10 @@
 set -euo pipefail
 v=$1
 
-commit_message="bump ${v}"
+commit_message="bump v${v}"
 
 sed -i "" "s/\(Version:\).*/\1 ${v}/" DESCRIPTION
 git add DESCRIPTION
 git commit -m "${commit_message}"
-git tag -a "v${v}" -m "bump v${v}"
+git tag -a "${v}" -m "bump v${v}"
+git push --follow-tags
