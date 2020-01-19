0bb65e427a6b51397e424e5e08d40b4f9e0094da
pdiakumis <pdiakumis@unimelb.edu.au>
2020-01-20 09:28:21 +1100

    SVs: add evaluation metrics tibble

--- nogit/sandbox.R
+++ nogit/sandbox.R
@@ -3,16 +3,23 @@
 # require(woofr)
 
 sample <- "p33"
-d1 <- glue::glue("~/Desktop/projects/umccr/woof_compare/test/data/umccrise_0.15.12/{sample}/final")
-d2 <- glue::glue("~/Desktop/projects/umccr/woof_compare/test/data/umccrise_0.15.6/{sample}/final")
+d1 <- glue::glue("~/Desktop/projects/umccr/woof/nogit/data/umccrise_0.15.12/{sample}/final")
+d2 <- glue::glue("~/Desktop/projects/umccr/woof/nogit/data/umccrise_0.15.6/{sample}/final")
 final1 <- bcbio_outputs(d1)
 final2 <- bcbio_outputs(d2)
 merge_bcbio_outputs(d1, d2, sample)
 
-
-
-um1 <- glue::glue("~/Desktop/projects/umccr/woof_compare/test/data/umccrise_0.15.12/{sample}/umccrised/{sample}")
-um2 <- glue::glue("~/Desktop/projects/umccr/woof_compare/test/data/umccrise_0.15.6/{sample}/umccrised/{sample}")
+um1 <- glue::glue("~/Desktop/projects/umccr/woof/nogit/data/umccrise_0.15.12/{sample}/umccrised/{sample}")
+um2 <- glue::glue("~/Desktop/projects/umccr/woof/nogit/data/umccrise_0.15.6/{sample}/umccrised/{sample}")
 umccrise_outputs(um1)
 umccrise_outputs(um2)
 merge_umccrise_outputs(um1, um2, sample)
+
+
+# Structural Variants
+
+manta_fnames <- list.files(c(d1, d2), pattern = "manta.vcf.gz$", recursive = TRUE, full.names = TRUE)
+f1 <- manta_fnames[1]
+f2 <- manta_fnames[2]
+
+manta_isec_stats(f1, f2, sample, "manta_bc")
