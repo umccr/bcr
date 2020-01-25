22aecf8061172a12384828dd351fa32124797846
pdiakumis <pdiakumis@unimelb.edu.au>
2020-01-25 16:28:46 +1100

    test sv reading funcs

--- tests/testthat/test_compare_counts.R
+++ tests/testthat/test_compare_counts.R
@@ -1,10 +1,22 @@
-context("test-counts")
+context("test-comparison")
 
-count_vars <- read_count_file(system.file("extdata", "count_vars.txt", package = "woofr"))
-eval_stats <- read_eval_file(system.file("extdata", "eval_stats.tsv", package = "woofr"))
+snv_count <- read_count_file(system.file("extdata", "snv/count_vars.txt", package = "woofr"))
+snv_eval <- read_snv_eval_file(system.file("extdata", "snv/eval_stats.tsv", package = "woofr"))
+sv_eval <- read_sv_eval_file(system.file("extdata", "sv/eval_metrics.tsv", package = "woofr"))
+sv_fpfn <- read_sv_fpfn_file(system.file("extdata", "sv/fpfn.tsv", package = "woofr"))
 
-test_that("count vars is integer", {
-  expect_equal(count_vars, 13222)
-  expect_equal(ncol(eval_stats), 18)
+test_that("snv count is correct format", {
+  expect_equal(snv_count, 13222)
 })
 
+test_that("snv eval is correct format", {
+  expect_equal(ncol(snv_eval), 18)
+})
+
+test_that("sv eval is correct format", {
+  expect_equal(ncol(sv_eval), 10)
+})
+
+test_that("sv fpfn is correct format", {
+  expect_equal(ncol(sv_fpfn), 6)
+})
