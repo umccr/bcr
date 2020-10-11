0e94a31e844618c0a16b7beebac561b616d81f8b
pdiakumis <pdiakumis@unimelb.edu.au>
2020-10-11 17:06:47 +1100

    add more filetypes

--- R/validate.R
+++ R/validate.R
@@ -1,18 +1,23 @@
 #' Guess Type of File
 #'
-#' Guesses type of file.
+#' Guesses type of file based on suffix.
 #'
-#' @param file File path.
+#' @param x File path.
 #' @return A character string: FASTQ, BAM, VCF or OTHER.
 #' @export
-guess_file_type <- function(file) {
+guess_file_type <- function(x) {
   dplyr::case_when(
-    grepl("fastq.gz$", file, ignore.case = TRUE) ~ "FASTQ",
-    grepl("fastq$", file, ignore.case = TRUE) ~ "FASTQ",
-    grepl("fq$", file, ignore.case = TRUE) ~ "FASTQ",
-    grepl("fq.gz$", file, ignore.case = TRUE) ~ "FASTQ",
-    grepl("bam$", file, ignore.case = TRUE) ~ "BAM",
-    grepl("vcf$", file, ignore.case = TRUE) ~ "VCF",
-    grepl("vcf.gz$", file, ignore.case = TRUE) ~ "VCF",
+    grepl("\\.bam$", x, ignore.case = TRUE) ~ "BAM",
+    grepl("\\.bai$", x, ignore.case = TRUE) ~ "BAMindex",
+    grepl("\\.fastq.gz$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fastq$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fq$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fq\\.gz$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("manifest\\.txt$", x, ignore.case = TRUE) ~ "Manifest",
+    grepl("\\.md5$", x, ignore.case = TRUE) ~ "MD5",
+    grepl("md5\\.txt$", x, ignore.case = TRUE) ~ "MD5txt",
+    grepl("\\.vcf$", x, ignore.case = TRUE) ~ "VCF_unz",
+    grepl("\\.vcf\\.gz$", x, ignore.case = TRUE) ~ "VCF_gz",
+    grepl("\\.tbi$", x, ignore.case = TRUE) ~ "VCFindex",
     TRUE ~ "OTHER")
 }
