df8374f38bdc0aeb56d7c22d7789b6502a001757
pdiakumis <pdiakumis@unimelb.edu.au>
2020-10-11 17:25:01 +1100

    add validation readr funcs

--- R/validate.R
+++ R/validate.R
@@ -21,3 +21,62 @@ guess_file_type <- function(x) {
     grepl("\\.tbi$", x, ignore.case = TRUE) ~ "VCFindex",
     TRUE ~ "OTHER")
 }
+
+#' Read fqtools output
+#'
+#' Reads output from fqtools.
+#'
+#' @param f Path to file output by fqtools.
+#'
+#' @return Tibble with two columns: validation verdict and input file name.
+#' @export
+read_fqtools <- function(f) {
+  x <- readr::read_lines(f, n_max = 1)
+  tibble::tibble(verdict = x, fname = sub("\\.fqtools_validate\\.txt", "", basename(f))) %>%
+    dplyr::mutate(verdict = ifelse(x == "OK", "OK", "ERROR"))
+}
+
+#' Read samtools quickcheck output
+#'
+#' Reads output from samtools quickcheck.
+#'
+#' @param f Path to file output by samtools quickcheck.
+#'
+#' @return Tibble with two columns: validation verdict and input file name.
+#' @export
+read_qcheck <- function(f) {
+  x <- readr::read_lines(f, n_max = 1)
+  tibble::tibble(verdict = x, fname = sub("\\.quickcheck\\.txt", "", basename(f))) %>%
+    dplyr::mutate(verdict = ifelse(x == "ok", "OK", "ERROR"))
+}
+
+#' Read bcftools query output
+#'
+#' Reads output from bcftools query for sample list.
+#'
+#' @param f Path to file output by bcftools query.
+#'
+#' @return Tibble with three columns: validation verdict, input file name, and basename.
+#' @export
+read_bcftools_querysn <- function(f) {
+  x <- readr::read_lines(f)
+  if (x[1] != "ok") {
+    stop(paste(f, "failed! Please check!"))
+  }
+  tibble::tibble(fname = sub("\\.query_sn\\.txt", "", basename(f))) %>%
+    dplyr::mutate(verdict = "OK") %>%
+    dplyr::select(verdict, fname)
+}
+
+#' Read md5sum output
+#'
+#' Reads output md5sum.
+#'
+#' @param f Path to md5sum file.
+#'
+#' @return Tibble with two columns: md5sum and basename of input file.
+#' @export
+read_md5sum <- function(f) {
+  readr::read_table2(f, col_names = c("md5sum", "fname"), col_types = "cc") %>%
+    dplyr::mutate(fname = basename(fname))
+}
