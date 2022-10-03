7aefdce49cea881e1592bb72f8a802ca34e09592
pdiakumis <peterdiakumis@gmail.com>
2022-10-04 01:35:59 +1100

    cleanup bcbio stuff

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2019 Peter Diakumis
+Copyright (c) 2022 Peter Diakumis
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- R/utils.R
+++ R/utils.R
@@ -29,3 +29,27 @@ tree <- function(x, ...) {
     system(paste0("tree ", ..., x), intern = TRUE),
     sep = "\n")
 }
+
+#' Guess Type of File
+#'
+#' Guesses type of file based on suffix.
+#'
+#' @param x File path.
+#' @return A character string: FASTQ, BAM, VCF or OTHER.
+#' @export
+guess_file_type <- function(x) {
+  dplyr::case_when(
+    grepl("\\.bam$", x, ignore.case = TRUE) ~ "BAM",
+    grepl("\\.bai$", x, ignore.case = TRUE) ~ "BAMindex",
+    grepl("\\.fastq.gz$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fastq$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fq$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("\\.fq\\.gz$", x, ignore.case = TRUE) ~ "FASTQ",
+    grepl("manifest\\.txt$", x, ignore.case = TRUE) ~ "Manifest",
+    grepl("\\.md5$", x, ignore.case = TRUE) ~ "MD5",
+    grepl("md5\\.txt$", x, ignore.case = TRUE) ~ "MD5txt",
+    grepl("\\.vcf$", x, ignore.case = TRUE) ~ "VCF_unz",
+    grepl("\\.vcf\\.gz$", x, ignore.case = TRUE) ~ "VCF_gz",
+    grepl("\\.tbi$", x, ignore.case = TRUE) ~ "VCFindex",
+    TRUE ~ "OTHER")
+}
